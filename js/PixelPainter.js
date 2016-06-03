224d7677d988fcd194ce0c20b30ecb1b9c34300e
Nikki K <808nikz@gmail.com>
2016-06-02 18:40:36 -1000

    return HTMLElement

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,3 +1,10 @@
-function createGrid(argument) {
-  // body...
+var createRows = document.createElement('div');
+createRows.className = 'newRow';
+document.getElementById('pixelPainter').appendChild(createRows);
+
+function createGrid(rows,columns,attributes) {
+  if(rows < 0 || typeof rows !== 'number') {
+    throw new Error();
+  }
+  return createRows;
 }
