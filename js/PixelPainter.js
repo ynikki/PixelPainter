7a33532982be9ecad985ef73bcc8c56f78275575
Nikki K <808nikz@gmail.com>
2016-06-02 19:49:41 -1000

    Returned number of rows

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,10 +1,17 @@
-var createRows = document.createElement('div');
-createRows.className = 'newRow';
-document.getElementById('pixelPainter').appendChild(createRows);
-
 function createGrid(rows,columns,attributes) {
   if(rows < 0 || typeof rows !== 'number') {
     throw new Error();
   }
-  return createRows;
+
+  var containElem = document.createElement('div');
+  containElem.className = 'containRow';
+
+  for(var i=0; i<rows; i++){
+    var firstChild = document.createElement('div');
+    firstChild.className = 'firstRow';
+    containElem.appendChild(firstChild);
+  }
+
+ return containElem;
+
 }
